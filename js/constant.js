cb6637d90c758254c0982522877a90b54910bd05
akbasliogulcan <ogulcanakbsli@gmail.com>
2025-12-21 18:01:52 +0300

    commits

---
+++ js/constant.js
@@ -0,0 +1,32 @@
+const personIcon = L.icon({
+           iconUrl: "../images/person.png",
+           iconSize: [50, 50],
+           iconAnchor: [25, 50],   // ALT ORTA
+});
+
+const homeIcon = L.icon({
+           iconUrl: "../images/home.png",
+           iconSize: [50, 50],
+           iconAnchor: [25, 50],
+});
+
+const jobIcon = L.icon({
+           iconUrl: "../images/briefcase.png",
+           iconSize: [50, 50],
+           iconAnchor: [25, 50],
+});
+
+const goToIcon = L.icon({
+           iconUrl: "../images/plane.png",
+           iconSize: [50, 50],
+           iconAnchor: [25, 50],
+});
+
+const parkIcon = L.icon({
+           iconUrl: "../images/park.png",
+           iconSize: [50, 50],
+           iconAnchor: [25, 50],
+});
+
+
+export { personIcon, homeIcon, jobIcon, goToIcon, parkIcon };
--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+import { personIcon } from "./constant.js";
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-           maxZoom: 19,
-           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+
+
+//*konum belirleme kullanıcıdan izin alarak
+window.navigator.geolocation.getCurrentPosition(
+           (e) => {
+
+                      //* kullanıcı konumu kabul ederse kendi konumu çıkar.
+                      loadMap([e.coords.latitude, e.coords.longitude]);
+           },
+           (e) => {
+                      //*eğer ki kullanıcını konumu redderse çıkacak olan konum
+                      loadMap([39.924655, 32.836576]);
+           }
+
+);
+
+
+
+
+
+
+//*Map oluşturan Fonks.  buradaki currentposition yukarıkida window içindeki e den gelir.
+function loadMap(currentPosition) {
+
+
+
+           var map = L.map('map').setView(currentPosition, 13);
+
+           L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                      maxZoom: 19,
+                      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+           }).addTo(map);
+
+           //*marker oluştur
+           L.marker(currentPosition, { icon: personIcon }).addTo(map);
+
+
+}
+
 
