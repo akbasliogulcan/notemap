b70fd48d916f209d2952b3363f5f43bcb76bc5b8
akbasliogulcan <ogulcanakbsli@gmail.com>
2025-12-24 14:13:36 +0300

    some commits

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { personIcon } from "./constant.js";
+import { homeIcon, personIcon } from "./constant.js";
 
 
 
@@ -7,26 +7,18 @@ window.navigator.geolocation.getCurrentPosition(
            (e) => {
 
                       //* kullanıcı konumu kabul ederse kendi konumu çıkar.
-                      loadMap([e.coords.latitude, e.coords.longitude]);
+                      loadMap([e.coords.latitude, e.coords.longitude], "Mevcut konum");
            },
            (e) => {
                       //*eğer ki kullanıcını konumu redderse çıkacak olan konum
-                      loadMap([39.924655, 32.836576]);
+                      loadMap([39.924655, 32.836576], "Varsayılan konum");
            }
 
 );
 
-
-
-
-
-
 //*Map oluşturan Fonks.  buradaki currentposition yukarıkida window içindeki e den gelir.
-function loadMap(currentPosition) {
-
-
-
-           var map = L.map('map').setView(currentPosition, 13);
+function loadMap(currentPosition, message) {
+           var map = L.map('map').setView(currentPosition, 11);
 
            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                       maxZoom: 19,
@@ -34,9 +26,17 @@ function loadMap(currentPosition) {
            }).addTo(map);
 
            //*marker oluştur
-           L.marker(currentPosition, { icon: personIcon }).addTo(map);
+           L.marker(currentPosition, { icon: homeIcon }).addTo(map).bindPopup(message);
 
+           //*harita üzerindeki tıklanma olaylarını izleyen fonks.
 
-}
+           map.on("click", onMapClick);
+};
+
+//*harita üzerindeki tıklanma olaylarını izleyen fonks.
+function onMapClick(e) {
+           const clickedCoords = [e.latlng.lat, e.latlng.lng];
+           console.log(clickedCoords);
+};
 
 
